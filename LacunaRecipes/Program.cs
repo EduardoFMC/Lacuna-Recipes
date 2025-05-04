95052f0a676d5241ad3a868335554d6d23ce3d78
Eduardo Ferreira <eduardo.ferreira.mc.edu@gmail.com>
2025-05-03 23:05:49 -0300

    dockering

--- LacunaRecipes/Program.cs
+++ LacunaRecipes/Program.cs
@@ -5,9 +5,10 @@ using LacunaRecipes.Entities;
 using Microsoft.EntityFrameworkCore;
 
 
-
 var builder = WebApplication.CreateBuilder(args);
+var env = builder.Environment;
 
+// Register services BEFORE builder.Build()
 builder.Services.AddScoped<IIngredientRepository, IngredientRepository>();
 builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
 builder.Services.AddScoped<IRecipeAndIngredientRepository, RecipeAndIngredientRepository>();
@@ -17,33 +18,33 @@ builder.Services.AddScoped<IngredientService>();
 builder.Services.AddScoped<RecipeService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-	options.UseSqlServer(
-		builder.Configuration.GetConnectionString("DefaultConnection")
-	)
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
 );
 
 builder.Services.AddControllers()
-	.AddJsonOptions(options => {
-		options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-	});
+    .AddJsonOptions(options => {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+using (var scope = app.Services.CreateScope()) {
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+// Swagger only in development
+if (env.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
